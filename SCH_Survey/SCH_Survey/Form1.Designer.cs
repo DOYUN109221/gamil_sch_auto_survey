d3f853d761a8b85d4cf171490432b003529e8061
DOYUN109221 <99240950+DOYUN109221@users.noreply.github.com>
2022-04-09 21:44:23 +0900

    Add files via upload

---
+++ SCH_Survey/SCH_Survey/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace SCH_Survey
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.sum = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.sum);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(178, 103);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // sum
+            // 
+            this.sum.AutoSize = true;
+            this.sum.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.sum.Location = new System.Drawing.Point(76, 60);
+            this.sum.Name = "sum";
+            this.sum.Size = new System.Drawing.Size(15, 16);
+            this.sum.TabIndex = 1;
+            this.sum.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("굴림", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(39, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 19);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "신청 횟수";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(6, 131);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(166, 103);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.checkBox3);
+            this.groupBox2.Controls.Add(this.checkBox2);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Controls.Add(this.richTextBox1);
+            this.groupBox2.Location = new System.Drawing.Point(12, 121);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(178, 240);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Checked = true;
+            this.checkBox3.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox3.Location = new System.Drawing.Point(8, 20);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(76, 16);
+            this.checkBox3.TabIndex = 6;
+            this.checkBox3.Text = "단일 신청";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(8, 42);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(76, 16);
+            this.checkBox2.TabIndex = 5;
+            this.checkBox2.Text = "차트 신청";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 71);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "단일 신청";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 116);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "차트 신청";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(8, 86);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(164, 21);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "뽀로로 아야 아파요";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(6, 38);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(193, 45);
+            this.trackBar1.TabIndex = 3;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Controls.Add(this.trackBar1);
+            this.groupBox3.Location = new System.Drawing.Point(196, 12);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(205, 349);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Enter += new System.EventHandler(this.groupBox3_Enter);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 24);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 12);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "쓰레드 : 0";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 5000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(413, 373);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.Text = "학교 신청곡 테러기";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label sum;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ SCH_Survey/SCH_Survey/Form1.cs
@@ -0,0 +1,180 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SCH_Survey
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public static bool checkboxbool = false;
+        public static bool[] working = { false, false, false, false, false, false, false, false, false, false };
+        public static int working2 = 0;
+        public static int suuuum = 0;
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            checkboxbool = checkBox3.Checked;
+            if (checkboxbool == false)
+            {
+                checkBox2.Checked = true;
+                checkboxbool = true;
+            }
+
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            checkboxbool = checkBox2.Checked;
+            if(checkboxbool == false)
+            {
+                checkBox3.Checked = true;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            trackBar1.Maximum = 10;
+            timer1.Start();
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            int thread = trackBar1.Value;
+
+            label4.Text = $"쓰레드 : {thread.ToString()}";
+            start(thread + 1);
+        }
+
+        public void start(int thread)
+        {
+            int workingsum = 0;
+            foreach (bool a in working)
+            {
+                if(a)
+                {
+                    ++workingsum;
+                }
+            }
+            
+            if (thread == 0)
+            {
+                working2 = 0;
+                for (int i = 0; i < working.Length; i++)
+                {
+                    working[i] = false;
+                }
+            }
+            else if (workingsum > thread)
+            {
+                for (int i = thread; i < working.Length; i++)
+                {
+                    working[i] = false;
+                    working2--;
+                }
+            }
+            else if(workingsum < thread)
+            {
+                for (int i = workingsum; i < thread; i++)
+                {
+                    working[i] = true;
+                }
+            }
+
+            workingsum = 0;
+            foreach (bool a in working)
+            {
+                if (a)
+                {
+                    ++workingsum;
+                }
+            }
+            if(workingsum > working2)
+            {
+                
+                for(int i = working2 - 1; i < workingsum; i++)
+                {
+                    Thread thread1 = new Thread(() => Run(i));
+                    thread1.Start();
+                }
+                
+            }
+
+        }
+
+
+        public void Run(int a)
+        {
+            try
+            {
+                Random ran = new Random();
+                int random = ran.Next(1, 10);
+                while (working[a])
+                {
+                    Console.WriteLine(a + " 작동중 고유번호: " + random);
+                    using(IWebDriver driver = new ChromeDriver())
+                    {
+                        driver.Url = "https://form.office.naver.com/form/responseView.cmd?formkey=NWNjYWU4ODctNWY5Ni00M2Y5LTk5YWItMWJmODAxMmVhYjJj&sourceId=urlshare";
+                        driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(3);
+                        driver.FindElement(By.XPath("/html/body/div[5]/div[1]/div[1]/div/div[2]/div/div/div[2]/form/div/div/div/div[3]/div/textarea")).SendKeys(getdata());
+                        Thread.Sleep(1000);
+                        driver.FindElement(By.XPath("/html/body/div[5]/div[1]/div[1]/div/div[2]/div/div/div[2]/div/button[3]")).Click();
+                        Thread.Sleep(1000);
+                        check();
+
+                    }
+                }
+            }
+            catch 
+            { 
+            }
+        }
+
+        public void check()
+        {
+            suuuum++;
+        }
+        public string getdata()
+        {
+            if(checkBox3.Checked)
+            {
+                if(textBox1.Text.Length >= 1)
+                {
+                    return textBox1.Text;
+                }
+                return "뽀로로 아야 아파요";
+            }
+            else
+            {
+                string[] rich = richTextBox1.Text.Split('\n');
+                if(rich.Length > suuuum)
+                {
+                    return rich[suuuum];
+                }
+                return "뽀로로 아야 아파요";
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            sum.Text = suuuum.ToString();
+        }
+
+        private void groupBox3_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
